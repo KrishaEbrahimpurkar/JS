07925d6d935c482220c99ef844379b0e2f2aabe2
Krisha Ebrahimpurkar <kebrahimpurkar@gmail.com>
2026-08-14 12:34:17 +0530

    file 5 strings

---
+++ basics/05_strings.js
@@ -0,0 +1,30 @@
+// const name = "Krisha ";
+// const repoCount = 3
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);//string interpolation
+
+const gameName = new String("Krisha-hc"); //string declaration 
+//console.log(gameName[0]); 
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());//original value does not change because it is stored in stack
+//console.log(gameName.charAt(5));
+//console.log(gameName.indexOf("h"));
+
+// const newString = gameName.substring(0,4)//substring does not obey negative values (way of slicing)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)//slice obeys negative values (also a technique of slicing)
+// console.log(anotherString);
+
+// const newString1 = "   krisha  "
+// console.log(newString1);
+// console.log(newString1.trim());//removes spaces 
+
+const url = "https://www.krisha.com/krisha%20ebrahimpurkar"
+//console.log(url.replace("%20" , "-"))
+//console.log(url.includes("krisha"))
+console.log(gameName.split("-"))
+
